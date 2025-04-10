e9d91a2cad7e37e9b68852bc8c01180da7f71854
MICHBONILLA <micbonilla91@gmail.com>
2025-04-10 15:40:03 -0500

    crud Django terminado

--- todo/urls.py
+++ todo/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('admin/', admin.site.urls),
     path("agregar/",views.agregar,name="agregar"),
-    
+    path("eliminar/<int:tarea_id>/",views.eliminar,name="eliminar"),
+    path("editar/<int:tarea_id>/",views.editar,name="editar"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -3,9 +3,9 @@ from .models import Tarea
 from .forms import TareaForm
 
 def home(request):
-    tareas=Tarea.objects.all()
-    context={'tareas':tareas}
-    return render(request,'todo/home.html',context)
+    tareas = Tarea.objects.all()
+    context = {'tareas': tareas}
+    return render(request, 'todo/home.html', context)
 
 def agregar(request):
     if request.method == "POST":
@@ -15,8 +15,24 @@ def agregar(request):
             return redirect('home')
     else:
         form = TareaForm()
-    
-    return render(request,'todo/agregar.html',{'form':form})
+    return render(request, 'todo/agregar.html', {'form': form})
+
+def eliminar(request, tarea_id):
+    tarea = Tarea.objects.get(id=tarea_id)
+    tarea.delete()
+    return redirect("home")
+
+def editar(request, tarea_id):
+    tarea = Tarea.objects.get(id=tarea_id)
+    if request.method == "POST":
+        form = TareaForm(request.POST, instance=tarea)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+    else:
+        form = TareaForm(instance=tarea)
+    context = {"form": form}
+    return render(request, 'todo/editar.html', context)
 
 
 # Create your views here.
